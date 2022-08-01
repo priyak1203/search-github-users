22440f3c3a90d9003e4eb0760cf79c4170963c91
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-01 11:19:09 +0530

    Example Chart with dynamic data

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,16 +1,65 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useGithubContext } from '../context/context';
 import { ExampleChart } from './Charts';
 
 const Repos = () => {
   const { repos } = useGithubContext();
   // console.log(repos);
+
+  // STEP 2 - Chart Data
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '59',
+    },
+    {
+      label: 'CSS',
+      value: '260',
+    },
+    {
+      label: 'JavaScript',
+      value: '180',
+    },
+    {
+      label: 'Python',
+      value: '40',
+    },
+  ];
+
   return (
-    <div>
-      Repos
-      <ExampleChart />
-    </div>
+    <section className="section">
+      <Wrapper className="section-center">
+        <ExampleChart data={chartData} />
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: pink;
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
 
 export default Repos;
