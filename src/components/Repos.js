5535487eac0a33600880b21f10fa7a7e58a08dc2
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-02 13:43:46 +0530

    Refactored Single Reduce
    use reduce function to calculat both stars and languages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -30,57 +30,48 @@ const Repos = () => {
     },
   ];
 
-  // Data for pie chart - most used languages
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  // extracting languages, no of repos and total star count per language
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+
     if (!language) return total;
 
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
 
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  // Data for pie chart - most used languages
+  const mostLanguages = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
 
   // Data for doughnut chart - most stars per language
-  let stars = repos.reduce((total, item) => {
-    const { language, stargazers_count } = item;
-    if (!language) return total;
-    if (!total[language]) {
-      total[language] = { label: language, value: stargazers_count };
-    } else {
-      total[language] = {
-        ...total[language],
-        value: total[language].value + stargazers_count,
-      };
-    }
-
-    return total;
-  }, {});
-
-  stars = Object.values(stars)
+  const mostStars = Object.values(languages)
     .sort((a, b) => {
-      return b.value - a.value;
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
     })
     .slice(0, 5);
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie data={languages} />
+        <Pie data={mostLanguages} />
         <ExampleChart data={chartData} />
-        <Doughnut data={stars} />
+        <Doughnut data={mostStars} />
       </Wrapper>
     </section>
   );
