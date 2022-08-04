426a41dde846f541b69d8c733a1aff8061752954
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-04 15:56:13 +0530

    Basic Auth0 Setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Domain = `${process.env.REACT_APP_DOMAIN}`;
+const ClientId = `${process.env.REACT_APP_CLIENT_ID}`;
+
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain={Domain}
+      clientId={ClientId}
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
