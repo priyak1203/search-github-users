034376028ac67fc620c8785740f73b6b1f1dbbce
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-05 16:14:05 +0530

    AuthWrapepr Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { Dashboard, Error, Login } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivateRoute from './pages/PrivateRoute';
+import AuthWrapper from './pages/AuthWrapper';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </AuthWrapper>
   );
 }
 
