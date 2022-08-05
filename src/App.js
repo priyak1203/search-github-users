c5ae85b4c334bd80f95aa6050602a855a152a1ab
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-05 15:24:01 +0530

    private route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Dashboard, Error, Login } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './pages/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 import styled from 'styled-components';
 import loginImg from '../images/login-img.svg';
 
 const Login = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Wrapper>
       <div className="container">
         <img src={loginImg} alt="github user" />
         <h1>github user</h1>
-        <button className="btn">log in / sign up</button>
+        <button className="btn" onClick={loginWithRedirect}>
+          log in / sign up
+        </button>
       </div>
     </Wrapper>
   );
